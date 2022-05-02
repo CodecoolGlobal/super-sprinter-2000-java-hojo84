e429ddcde1c1dd8cb769b50069e29dffae3a6e7c
hojo84 <hojo84@gmail.com>
2022-05-02 07:12:19 +0200

    Create class SuperSprinter2000Application with main() method

---
+++ src/main/java/com/codecool/supersprinter/SuperSprinter2000Application.java
@@ -0,0 +1,12 @@
+package com.codecool.supersprinter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperSprinter2000Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperSprinter2000Application.class, args);
+    }
+}
