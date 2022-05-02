16c40e6f1754f0ef46f2ffb49563429fd52d0ad7
hojo84 <hojo84@gmail.com>
2022-05-02 17:22:49 +0200

    Create schema.sql and data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO user_story (story_title, user_story, acceptance_criteria, business_value, status)
+VALUES ('Create submit button', 'As a user, I want to submit user stories with a button', 'Some acceptance criteria', 8,
+        'Draft');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS user_story CASCADE;
+DROP SEQUENCE IF EXISTS user_story_id_seq;
+
+CREATE TABLE user_story
+(
+    id                  long identity NOT NULL PRIMARY KEY,
+    story_title         varchar(100) NOT NULL,
+    user_story          varchar(400) NOT NULL,
+    acceptance_criteria varchar(400) NOT NULL,
+    business_value      double,
+    status              varchar(20)
+);
