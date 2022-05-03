a27ea2691a4f59532effd317cc25b3c8a99a723b
hojo84 <hojo84@gmail.com>
2022-05-03 07:13:18 +0200

    Add char length constraint for story title in schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,7 +4,7 @@ DROP SEQUENCE IF EXISTS user_story_id_seq;
 CREATE TABLE user_story
 (
     id                  long identity NOT NULL PRIMARY KEY,
-    story_title         varchar(100) NOT NULL,
+    story_title         varchar(100) NOT NULL CHECK (CHAR_LENGTH(story_title) >= 5 ),
     user_story          varchar(400) NOT NULL,
     acceptance_criteria varchar(400) NOT NULL,
     business_value      double,
