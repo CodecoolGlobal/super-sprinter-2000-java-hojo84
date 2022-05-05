160f6667509d8172cfb7920e293673721d80f975
hojo84 <hojo84@gmail.com>
2022-05-05 07:20:01 +0200

    Implement 'Add new User Story' feature

--- src/main/java/com/codecool/supersprinter/controller/UserStoryController.java
+++ src/main/java/com/codecool/supersprinter/controller/UserStoryController.java
@@ -1,12 +1,17 @@
 package com.codecool.supersprinter.controller;
 
+import com.codecool.supersprinter.model.UserStory;
 import com.codecool.supersprinter.service.UserStoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/")
 public class UserStoryController {
 
     private final UserStoryService userStoryService;
@@ -22,4 +27,21 @@ public class UserStoryController {
         return "index";
     }
 
+    @GetMapping("/story")
+    public String displayAddNewUserStoryForm() {
+        return "story";
+    }
+
+    @PostMapping
+    public String addNewUserStory(@RequestParam(name = "title") String title, @RequestParam(name = "userStory") String userStory,
+                                  @RequestParam(name = "acceptanceCriteria") String acceptanceCriteria,
+                                  @RequestParam(name = "businessValue") double businessValue) {
+        UserStory newUserStory = new UserStory();
+        newUserStory.setTitle(title);
+        newUserStory.setUserStory(userStory);
+        newUserStory.setAcceptanceCriteria(acceptanceCriteria);
+        newUserStory.setBusinessValue(businessValue);
+        userStoryService.addUserStory(newUserStory);
+        return "redirect:/";
+    }
 }
