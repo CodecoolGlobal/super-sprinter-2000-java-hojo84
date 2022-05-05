103cdaa614d3029b3db152700d7742a6ab95b6da
hojo84 <hojo84@gmail.com>
2022-05-05 14:13:26 +0200

    Change status values in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO user_story (story_title, user_story, acceptance_criteria, business_value, status)
 VALUES ('Create submit button', 'As a user, I want to submit user stories with a button', 'Some acceptance criteria', 8,
-        'Draft'),
+        'planning'),
        ('List all user stories', 'As a user, I want to see all previously submitted user stories',
         'When opening the website user stories should show up & When opening the website user stories should show upWhen opening the website user stories should show up', 4,
-        'In progress');
+        'review');
